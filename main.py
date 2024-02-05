f8b980584d7e7c953925d0834c56b03c96598941
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-05 13:41:20 -0800

    Add command line arguments and headless mode

--- main.py
+++ main.py
@@ -2,8 +2,29 @@ import selenium
 import undetected_chromedriver
 import time
 import requests
+import sys
 
-driver = undetected_chromedriver.Chrome(driver_executable_path="C:\\Users\\Thomas\\OneDrive\\Apps\\Documents\\GitHub\\Narcissus\\Narcisuss\\chromedriver.exe")
-while True:
-    driver.get("https://github.com/ThomasGust/ThomasGust")
-    time.sleep(1)
+
+if __name__ == "__main__":
+    options = uc.ChromeOptions()
+    if sys.argv[4] == 1:
+        options.add_argument("--headless")
+
+    username = sys.argv[1]
+    refresh_lag = sys.argv[2]
+    profile_path = f"https://github.com/{username}/{username}"
+    n = sys.argv[3]
+
+    driver = undetected_chromedriver.Chrome(driver_executable_path="chromedriver.exe", options=options, use_subprocess=True)
+
+    counted = 0
+
+    if n == 0:
+        while True:
+            driver.get(profile_path)
+            time.sleep(refresh_lag)
+    else:
+        while counted <= n:
+            driver.get(profile_path)
+            time.sleep(refresh_lag)
+
