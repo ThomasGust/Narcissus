8159a9d080d83f94f7c4427cab09f51cd5fce9f2
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-05 15:41:39 -0800

    Edits

---
+++ driver.py
@@ -0,0 +1,51 @@
+import selenium
+import undetected_chromedriver
+import time
+import requests
+import sys
+
+"""
+This is a simple toy script to give a GitHub profile more views. Command line usage will be described in the README for this repository.
+"""
+print("STARTED")
+options = undetected_chromedriver.ChromeOptions()
+if sys.argv[4] == 1:
+    options.add_argument("--headless")
+
+username = sys.argv[1]
+refresh_lag = int(sys.argv[2])
+profile_path = f"https://github.com/{username}/{username}"
+n = int(sys.argv[3])
+
+driver = undetected_chromedriver.Chrome(driver_executable_path="chromedriver.exe", options=options)
+driver.get(profile_path)
+
+counted = 0
+
+if n == 0:
+    while True:
+        try:
+            #driver.get(profile_path)
+            driver.execute_script("location.reload()")
+            time.sleep(refresh_lag)
+        except Exception as e:
+            if type(e) != KeyboardInterrupt:
+                driver = undetected_chromedriver.Chrome(driver_executable_path="chromedriver.exe", options=options, use_subprocess=True)
+                driver.get(profile_path)
+                time.sleep(refresh_lag)
+            else:
+                break
+else:
+    while counted <= n:
+        try:
+            #driver.get(profile_path)
+            driver.execute_script("location.reload()")
+            time.sleep(refresh_lag)
+        except Exception as e:
+            if type(e) != KeyboardInterrupt:
+                driver = undetected_chromedriver.Chrome(driver_executable_path="chromedriver.exe", options=options, use_subprocess=True)
+                driver.get(profile_path)
+                time.sleep(refresh_lag)
+            else:
+                break
+        counted += 1
--- main.py
+++ main.py
@@ -1,46 +1,5 @@
-import selenium
-import undetected_chromedriver
-import time
-import requests
-import sys
+import os
+print("HELLO")
 
-
-if __name__ == "__main__":
-    options = undetected_chromedriver.ChromeOptions()
-    if sys.argv[4] == 1:
-        options.add_argument("--headless")
-
-    username = sys.argv[1]
-    refresh_lag = int(sys.argv[2])
-    profile_path = f"https://github.com/{username}/{username}"
-    n = int(sys.argv[3])
-
-    driver = undetected_chromedriver.Chrome(driver_executable_path="chromedriver.exe", options=options, use_subprocess=True)
-
-    counted = 0
-
-    if n == 0:
-        while True:
-            try:
-                driver.get(profile_path)
-                time.sleep(refresh_lag)
-            except Exception as e:
-                if type(e) != KeyboardInterrupt:
-                    driver = undetected_chromedriver.Chrome(driver_executable_path="chromedriver.exe", options=options, use_subprocess=True)
-                    driver.get(profile_path)
-                    time.sleep(refresh_lag)
-                else:
-                    break
-    else:
-        while counted <= n:
-            try:
-                driver.get(profile_path)
-                time.sleep(refresh_lag)
-            except Exception as e:
-                if type(e) != KeyboardInterrupt:
-                    driver = undetected_chromedriver.Chrome(driver_executable_path="chromedriver.exe", options=options, use_subprocess=True)
-                    driver.get(profile_path)
-                    time.sleep(refresh_lag)
-                else:
-                    break
-            counted += 1
+for i in range(10):
+    os.system("python driver.py ThomasGust 1 0 1")
